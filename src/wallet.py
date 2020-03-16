b6be001bf8c1ab34e8f65f23a8134cf4ef73e750
ThanosM97 <thanosmas97@gmail.com>
2020-03-16 17:43:36 +0200

    Convert key bytes to string

--- src/wallet.py
+++ src/wallet.py
@@ -29,10 +29,10 @@ class Wallet:
         # Generate a private key of key length of 1024 bits.
         key = RSA.generate(1024)
 
-        self.private_key = key.exportKey()
+        self.private_key = key.exportKey().decode('ISO-8859-1')
 
         # Generate the public key from the above private key.
-        self.public_key = key.publickey().exportKey()
+        self.public_key = key.publickey().exportKey().decode('ISO-8859-1')
         self.transactions = []
 
     def __str__(self):
