84bc3395282a973226edf5defa9b54638c0c5570
ThanosM97 <thanosmas97@gmail.com>
2020-03-25 17:06:22 +0200

    Add node app

---
+++ webapp/node/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Node(models.Model):
+    id = models.IntegerField(primary_key=True)
+    node_id = models.IntegerField(null=True, blank=True)
+    IP = models.GenericIPAddressField()
+    PORT = models.IntegerField(default=5000)
+    status = models.TextField(max_length=7, default='Offline')
+
+    def __str__(self):
+        return f'Node {self.node_id}'
+    
+    def get_absolute_url(self):
+        return reverse('node-new-transaction')
+
+class Transaction(models.Model):
+    receiver_id = models.IntegerField()
+    amount = models.IntegerField()
+
+    def get_absolute_url(self):
+        return reverse('node-balance')
+
+
+
+
+    
---
+++ webapp/node/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Node
+from django.contrib import messages
+from .forms import CreateNodeForm, CreateTransactionForm
+from django.utils.safestring import mark_safe
+import requests
+
+@login_required
+def new_node(request):
+    if request.method == "POST":
+        form = CreateNodeForm(request.POST)
+        if form.is_valid():
+            IP = request.POST["IP"]
+            PORT = request.POST["PORT"]
+            try:
+                response = requests.get(f'http://{IP}:{PORT}/api/get_id')
+                node_id = response.json()["message"]
+                # ADD LINK TO NODE
+                messages.success(request, mark_safe(f'Node {node_id} is online. <a href="#">Start Node</a>'))
+                return redirect('node-new')
+            except:
+                messages.error(request, 'The node is not currently online.')
+                return redirect('node-new')
+    else:
+        form = CreateNodeForm()
+    return render(request, 'node/node_form.html', {'form': form})
+
+@login_required
+def new_transaction(request):
+    if request.method == "POST":
+        form = CreateTransactionForm(request.POST)
+        if form.is_valid():
+            # ADD REQUEST FOR TRANSACTION
+            try:
+                response = requests.get('')
+                messages.success(request, f'The transaction was successful.')
+                return redirect('node-balance')
+            except:
+                messages.error(request, f'Something went wrong. Please try again later.')
+                return redirect('node-new-transaction')                            
+    else:
+        form = CreateTransactionForm()
+    return render(request, 'node/new_transaction.html', {'form': form})
