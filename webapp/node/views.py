7ae5c7927c1343f2104fe331ffe1fd11d401c031
ThanosM97 <thanosmas97@gmail.com>
2020-03-27 01:43:37 +0200

    Update new transaction view

--- webapp/node/views.py
+++ webapp/node/views.py
@@ -27,21 +27,43 @@ def new_node(request):
     return render(request, 'node/node_form.html', {'form': form})
 
 @login_required
-def new_transaction(request):
+def new_transaction(request, id):
+    # There should be only one node satisfying the filtering statement below.
+    nodes = Node.objects.filter(id = id , status = "Online")
+    if not nodes:
+        messages.error(request, 'The node is not currently online.')
+        return redirect('noobcash-home')
+
+    # Getting the object from the queryset
+    node = nodes[0]
     if request.method == "POST":
         form = CreateTransactionForm(request.POST)
         if form.is_valid():
-            # ADD REQUEST FOR TRANSACTION
+            IP = node.IP
+            PORT = node.PORT
+            transaction = {
+                "receiver": request.POST["receiver_id"],
+                "amount": request.POST["amount"]
+            }            
             try:
-                response = requests.get('')
-                messages.success(request, f'The transaction was successful.')
-                return redirect('node-balance')
+                response = requests.post(f'http://{IP}:{PORT}/api/create_transaction', data=transaction)
+                data = response.json()
+                message = data["message"]
+                try:
+                    balance = data["balance"]
+                    if response.status_code == 200:
+                        messages.success(request, message)
+                    else:
+                        messages.error(request, message)
+                    messages.info(request, f'Your current balance is: {balance} NBCs')                
+                except KeyError:
+                    messages.error(request, message)
             except:
-                messages.error(request, f'Something went wrong. Please try again later.')
-                return redirect('node-new-transaction')                            
+                messages.error(request, f'Node is not active. Try again later.')
+            return redirect('node-new-transaction', node.id)                            
     else:
         form = CreateTransactionForm()
-    return render(request, 'node/new_transaction.html', {'form': form})
+    return render(request, 'node/new_transaction.html', {'form': form, 'node':node})
 
 @login_required
 def my_balance(request,id):
